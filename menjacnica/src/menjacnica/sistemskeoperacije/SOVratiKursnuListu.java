78b5df851484692435c73df378b1f801eaeeecb3
oliverakordic <oljaitacka@gmail.com>
2016-05-11 09:11:04 +0200

    Uvedene su sistemske operacije za sve metode klase Menjacnica.

---
+++ menjacnica/src/menjacnica/sistemskeoperacije/SOVratiKursnuListu.java
@@ -0,0 +1,11 @@
+package menjacnica.sistemskeoperacije;
+
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
+
+public class SOVratiKursnuListu {
+	public static LinkedList<Valuta> izvrsi (LinkedList<Valuta> kursnaLista){
+		return kursnaLista;
+	}
+}
